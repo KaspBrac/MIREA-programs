0ff52776b7e7993390b331d497341483933975e1
KaspBrac <121407635+KaspBrac@users.noreply.github.com>
2022-12-28 00:08:36 +0300

    Update 19.jl

--- Julia/19.jl
+++ Julia/19.jl
@@ -1,4 +1,4 @@
-function putmarker_at_border_and_back!(robot::Robot, side::HorizonSide, n_steps::Int = 0)
+function putmarker_at_border_and_back!(robot::Robot, side::HorizonSide, n_steps::Int = 0)#двигает робота до перегородки, ставит маркер и возвращает в исходную точку
     if !isborder(r, side)
         move!(r, side)
         n_steps += 1
@@ -9,12 +9,12 @@ function putmarker_at_border_and_back!(robot::Robot, side::HorizonSide, n_steps:
     end
 end
 
-function inverse_side(side::HorizonSide)::HorizonSide
+function inverse_side(side::HorizonSide)::HorizonSide #инверсирование сторон света
     inv_side = HorizonSide((Int(side) + 2) % 4)
     return inv_side
 end
 
-function along!(robot, side, num_steps)
+function along!(robot, side, num_steps)#движение до упора
     n_steps = 0
     while !isborder(robot, side) && n_steps < num_steps
         move!(robot, side)
