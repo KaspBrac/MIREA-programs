ec99a6b876e9fe284a58bf78c25f738aa6c91e33
KaspBrac <121407635+KaspBrac@users.noreply.github.com>
2022-12-28 00:00:03 +0300

    Update 2.jl

--- Julia/2.jl
+++ Julia/2.jl
@@ -1,15 +1,15 @@
-function mark_perimetr!(r::Robot)#задача 2
+function mark_perimetr!(r::Robot)#закрашивание периметра
     steps_ld = [0, 0] # (шаги_вниз, шаги_влево)
     steps_ld[1] = move_until_border!(r, Sud)
     steps_ld[2] = move_until_border!(r, West)
     for side in (Nord, Ost, Sud, West)
-        putmarkers_until_border!(r, side)
+        putmarkers_until_border!(r, side)#ставит маркер до тех пор, пока не упрётся в перегородку
     end
     moves!(r, Ost, steps_ld[2])
     moves!(r, Nord, steps_ld[1])
 end
 
-function move_until_border!(r::Robot, side::HorizonSide)::Int
+function move_until_border!(r::Robot, side::HorizonSide)::Int #движится,пока не упрётся в перегородку
     n_steps = 0
     while !isborder(r, side)
         n_steps += 1
@@ -31,5 +31,5 @@ end
 r=Robot("pole69.sit"; animate=true)
 
 mark_perimetr!(r)
-Footer
+
 
